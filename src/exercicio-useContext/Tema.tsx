e3e2a9c81a161979a0c4aa044d2ce31879369fee
Vinicius Felipe <viniciusfelipeff@gmail.com>
2025-10-01 09:52:04 -0300

    c

--- src/exercicio-useContext/Tema.tsx
+++ src/exercicio-useContext/Tema.tsx
@@ -2,9 +2,7 @@ import { useContext } from "react";
 import { ThemeContext } from "./ThemeContext";
 
 export default function Tema() {
-  const context = useContext(ThemeContext);
-  if (!context) throw new Error("Tema deve ser usado dentro de um ThemeProvider");
-  const { tema, alternarTema } = context;
+  const { tema, alternarTema } = useContext(ThemeContext);
 
   return (
     <div style={{ background: tema === "light" ? "#fff" : "#333", color: tema === "light" ? "#000" : "#fff" }}>
